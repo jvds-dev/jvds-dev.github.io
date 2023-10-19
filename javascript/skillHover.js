4457f9058d08584d5a5f61c83b83dba6b6a23bdf
JVDS <71608479+JVDS2@users.noreply.github.com>
2023-10-19 15:42:14 -0300

    skills section

---
+++ javascript/skillHover.js
@@ -0,0 +1,23 @@
+const elements = document.querySelectorAll('.skill');
+
+elements.forEach((element) => {
+    let canHover = true;
+
+    element.addEventListener('mouseenter', ()=> {
+        if(canHover){
+            element.classList.add('hovered');
+        }
+    })
+
+    element.addEventListener('mouseleave', ()=> {
+        setTimeout(()=>{
+            element.classList.remove('hovered');
+        },200)
+        canHover = true
+        // setTimeout(()=>{
+        //     canHover = true;
+        // }, 100)
+    })
+});
+
+
